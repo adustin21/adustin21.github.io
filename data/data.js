92cb967a64e18bb0ff8a6b37bf049abb1859c774
adustin21 <73604357+adustin21@users.noreply.github.com>
2021-01-04 00:37:22 +0300

    Update data.js

--- data/data.js
+++ data/data.js
@@ -2,7 +2,7 @@ const STORE = {
 	pageSettings: {
 		name: "adustin",
 		colorscheme: "dark",
-		logo: "https://instagram.fppk1-1.fna.fbcdn.net/v/t51.2885-15/e35/115729328_892234564600789_5276454620485538371_n.jpg?_nc_ht=instagram.fppk1-1.fna.fbcdn.net&_nc_cat=101&_nc_ohc=qVlnwzO7C4AAX9FU-Ns&tp=18&oh=895697bb8f0b1aa2ded5bfb8da2d8cd2&oe=5FD504C6"
+		logo: "https://scontent-arn2-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/110214189_604977687115950_6038916004280938289_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com&_nc_cat=106&_nc_ohc=uGWhQvxQipEAX9LlGja&tp=1&oh=db0773b3802835f5c9c2eeffa404558e&oe=601B2F29"
 	},
 	links:[
 		{
